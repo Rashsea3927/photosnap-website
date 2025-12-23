57f128f51ef48b0948f551224a73250e628433ae
Rashsea <kaji.dev3927@gmail.com>
2025-12-23 20:52:49 +0900

    feat: integrate Header component into RootLayout for improved site structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import type { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google';
 import './globals.css';
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${dmSans.className} antialiased`}>{children}</body>
+      <body className={`${dmSans.className} antialiased`}>
+        <Header />
+        <main className='relative'>{children}</main>
+      </body>
     </html>
   );
 }
